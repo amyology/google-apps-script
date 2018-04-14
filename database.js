a81aabad7cf69dbee0d006a14159736e3fb1e10c
Amy Chan <chamy09@gmail.com>
2018-04-14 14:33:49 -0500

    Change jdbcresultset formatting

--- database.js
+++ database.js
@@ -1,20 +1,31 @@
+var props = PropertiesService.getScriptProperties(),
+    dbInstance = 'jdbc:google:mysql://' + props.getProperty('db-url'),
+    dbUrl = dbInstance + '/' + props.getProperty('db-name'),
+    dbUser = props.getProperty('db-user'),
+    dbPwd = props.getProperty('db-pwd'),
+    connection = Jdbc.getCloudSqlConnection(dbUrl, dbUser, dbPwd);
+
 function runQuery () {
-  var props = PropertiesService.getScriptProperties(),
-      dbUrl = props.getProperty('db-url'),
-      dbUser = props.getProperty('db-user'),
-      dbPwd = props.getProperty('db-pwd'),
-      connection = Jdbc.getCloudSqlConnection(dbUrl, dbUser, dbPwd),
-      statement = connection.createStatement(),
+  var statement = connection.createStatement(),
       result = statement.executeQuery('SELECT * FROM project'),
-      resultArray = [];
+      numCols = result.getMetaData().getColumnCount(),
+      resultArray = [],
+      rowCount = 0;
   
   while(result.next()) {
-    resultArray.push({
-      'id': result.getString(1),
-      'name': result.getString(2),
-      'version': result.getString(3)
-    });
+    resultArray.push({});
+    for (var i = 1; i <= numCols; i++) {
+      resultArray[rowCount][result.getMetaData().getColumnName(i)] = result.getString(i);
+    }
+    rowCount++;
   }
   
   Logger.log(resultArray);
 }
+
+//function create () {
+//  var statement = connection.prepareStatement('INSERT INTO project ' + '(id, name) values (?, ?)');
+//  stmt.setString(1, 1);
+//  stmt.setString(2, 'Cocoa Puffs');
+//  stmt.execute();
+//}
